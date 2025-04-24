3979a7505334b9313d3c1f4954f15bea4a6489f9
MikoNyaa <itzmexanik.dnd@gmail.com>
2025-04-24 05:51:48 +0530

    Doin some shit in React

--- DBMS PBL/placement_portal/companies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.company_register, name='company_register'),
-    path('dashboard/', views.company_dashboard, name='company_dashboard'),
-    path('post-job/', views.post_job, name='post_job'),
-    path('applicants/<int:job_id>/', views.view_applicants, name='view_applicants'),
-]
